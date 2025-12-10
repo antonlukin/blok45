7e5845f59a28eb97221e0b3e8760a629311b188d
Anton Lukin <anton@lukin.me>
2025-12-10 21:42:16 +0100

    Add imgproxy module to compress images

--- public/modules/images.php
+++ public/modules/images.php
@@ -11,6 +11,8 @@ if ( ! defined( 'WPINC' ) ) {
 }
 
 class Blok45_Modules_Images {
+	private static $imgproxy_url = null;
+
 	/**
 	 * Use this method instead of constructor to avoid multiple hook setting
 	 */
@@ -22,6 +24,11 @@ class Blok45_Modules_Images {
 
 		add_filter( 'wp_generate_attachment_metadata', array( __CLASS__, 'compress_original_image' ), 10, 2 );
 		add_filter( 'wp_image_editors', array( __CLASS__, 'change_image_editor' ) );
+
+		if ( defined( 'BLOK45_IMGPROXY_URL' ) ) {
+			add_filter( 'wp_get_attachment_image', array( __CLASS__, 'add_imgproxy_attachments' ), 10, 5 );
+			add_filter( 'wp_content_img_tag', array( __CLASS__, 'add_imgproxy_content' ), 10, 3 );
+		}
 	}
 
 	/**
@@ -91,6 +98,127 @@ class Blok45_Modules_Images {
 
 		$wp_query->set_404();
 		status_header( 404 );
+	}
+
+	/**
+	 * Update attachments html to use picture with imgproxied urls
+	 */
+	public static function add_imgproxy_attachments( $html, $attachment_id, $size, $icon, $attr ) {
+		if ( is_admin() || ! $html || $icon ) {
+			return $html;
+		}
+
+		$srcset = isset( $attr['srcset'] ) ? $attr['srcset'] : '';
+
+		$webp_srcset = self::convert_srcset( $srcset, 'webp' );
+		$avif_srcset = self::convert_srcset( $srcset, 'avif' );
+
+		if ( empty( $srcset ) && $attr['src'] ) {
+			$webp_srcset = self::append_format( $attr['src'], 'webp' );
+			$avif_srcset = self::append_format( $attr['src'], 'avif' );
+		}
+
+		$picture = sprintf(
+			'<picture>
+                <source type="image/avif" srcset="%s"%s />
+                <source type="image/webp" srcset="%s"%s />
+                %s
+            </picture>',
+			esc_attr( $avif_srcset ),
+			isset( $attr['sizes'] ) ? sprintf( ' sizes="%s"', esc_attr( $attr['sizes'] ) ) : '',
+			esc_attr( $webp_srcset ),
+			isset( $attr['sizes'] ) ? sprintf( ' sizes="%s"', esc_attr( $attr['sizes'] ) ) : '',
+			$html
+		);
+
+		return $picture;
+	}
+
+	/**
+	 * Update images from content with imgproxied urls
+	 */
+	public static function add_imgproxy_content( $img, $context, $attachment_id ) {
+		if ( is_admin() || ! $attachment_id ) {
+			return $img;
+		}
+
+		$size = $context['size'] ?? 'full';
+
+		$src    = wp_get_attachment_image_url( $attachment_id, $size );
+		$srcset = wp_get_attachment_image_srcset( $attachment_id, $size );
+		$sizes  = wp_get_attachment_image_sizes( $attachment_id, $size );
+
+		$webp_srcset = self::convert_srcset( $srcset, 'webp' );
+		$avif_srcset = self::convert_srcset( $srcset, 'avif' );
+
+		if ( empty( $srcset ) && $src ) {
+			$webp_srcset = self::append_format( $src, 'webp' );
+			$avif_srcset = self::append_format( $src, 'avif' );
+		}
+
+		$picture = sprintf(
+			'<picture>
+                <source type="image/avif" srcset="%s"%s />
+                <source type="image/webp" srcset="%s"%s />
+                %s
+            </picture>',
+			esc_attr( $avif_srcset ),
+			$sizes ? sprintf( ' sizes="%s"', esc_attr( $sizes ) ) : '',
+			esc_attr( $webp_srcset ),
+			$sizes ? sprintf( ' sizes="%s"', esc_attr( $sizes ) ) : '',
+			$img
+		);
+
+		return $picture;
+	}
+
+	/**
+	 * Add format imgproxy url
+	 */
+	protected static function append_format( $url, $format ) {
+		$parsed_url = wp_parse_url( $url );
+
+		if ( empty( $parsed_url['path'] ) ) {
+			return $url;
+		}
+
+		$uploads_pos = strpos( $parsed_url['path'], '/uploads/' );
+
+		if ( $uploads_pos === false ) {
+			return $url;
+		}
+
+		$relative_path = substr( $parsed_url['path'], $uploads_pos + strlen( '/uploads/' ) );
+		$encoded_parts = array_map( 'rawurlencode', explode( '/', $relative_path ) );
+
+		// Compose imgproxy url
+		$imgproxy_url = BLOK45_IMGPROXY_URL . '/format:%s/plain/%s';
+
+		return sprintf( $imgproxy_url, $format, implode( '/', $encoded_parts ) );
+	}
+
+	/**
+	 * Smart method to convert srcset attribute
+	 */
+	protected static function convert_srcset( $srcset_str, $format ) {
+		$converted = array();
+
+		if ( empty( $srcset_str ) ) {
+			return '';
+		}
+
+		$sources = array_map( 'trim', explode( ',', $srcset_str ) );
+
+		foreach ( $sources as $source ) {
+			$parts      = preg_split( '/\s+/', $source, 2 );
+			$url        = $parts[0];
+			$descriptor = $parts[1] ?? '';
+			$updated    = self::append_format( $url, $format );
+
+			$converted[] = $descriptor ? ( $updated . ' ' . $descriptor ) : $new_url;
+		}
+
+		return implode( ', ', $converted );
 	}
 }
 
