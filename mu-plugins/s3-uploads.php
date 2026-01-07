f605e0ee28677d427760e524b0b9d95f8a5e09f0
Anton Lukin <anton@lukin.me>
2026-01-07 22:14:24 +0100

    Update s3-uploads mu-plugin to align code style

--- mu-plugins/s3-uploads.php
+++ mu-plugins/s3-uploads.php
@@ -1,18 +1,39 @@
 <?php
 /**
  * Plugin Name: S3 Uploads tweaks
- * Description: 
+ * Description: Adjusts the S3 Uploads client to work with DigitalOcean Spaces settings.
  * Author: Anton Lukin
- * Author URI: https://lukin.ne
+ * Author URI: https://lukin.me
  */
+
+declare( strict_types=1 );
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
-// Filter S3 Uploads params.
-add_filter( 's3_uploads_s3_client_params', function ( $params ) {
-	$params['endpoint'] = 'https://ams3.digitaloceanspaces.com';
-	$params['use_path_style_endpoint'] = false;
-	$params['debug'] = false; // Set to true if uploads are failing.
-	return $params;
-} );
+
+final class Blok45_S3_Uploads_Tweaks {
+	/**
+	 * Bootstrap static hooks.
+	 */
+	public static function init(): void {
+		add_filter( 's3_uploads_s3_client_params', array( __CLASS__, 'configure_s3_client' ) );
+	}
+
+	/**
+	 * Ensure the DigitalOcean Spaces endpoint and other client params are set.
+	 *
+	 * @param array $params API client arguments.
+	 *
+	 * @return array
+	 */
+	public static function configure_s3_client( array $params ): array {
+		$params['endpoint'] = 'https://ams3.digitaloceanspaces.com';
+		$params['use_path_style_endpoint'] = false;
+		$params['debug'] = false;
+
+		return $params;
+	}
+}
+
+Blok45_S3_Uploads_Tweaks::init();
